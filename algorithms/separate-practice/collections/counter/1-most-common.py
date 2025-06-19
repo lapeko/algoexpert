eaa19ceb00909350f7b56a44688e2608a09ba803
lapeko <lapeko88@gmail.com>
2025-06-19 19:27:37 +0200

    implement most common search

---
+++ algorithms/separate-practice/collections/counter/1-most-common.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+def get_most_common(chars):
+    return Counter(chars).most_common(1)[0][0]
+
+
+def run_tests():
+    tests = [{
+        "payload": ["a", "b", "a", "c", "b", "a"],
+        "expected": "a"
+    }]
+
+    for idx, t in enumerate(tests):
+        result = get_most_common(t["payload"])
+        print(f"Test: {idx + 1}")
+        if result == t["expected"]:
+            print("OK")
+        else:
+            print(f"Received \"{result}\" when \"{t["expected"]}\" expected")
+
+run_tests()
