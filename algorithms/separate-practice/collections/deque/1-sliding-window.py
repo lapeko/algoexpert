709eecb8196ad6e75acccbe42b15efae1dcd1552
lapeko <lapeko88@gmail.com>
2025-06-19 12:31:06 +0200

    implement sliding window with deque with O(N) instead of O(N * K)

--- algorithms/separate-practice/collections/deque/1-sliding-window.py
+++ algorithms/separate-practice/collections/deque/1-sliding-window.py
@@ -1,33 +1,67 @@
 from collections import deque
 
+# def get_max_item_in_window(nums, k):
+#     if k < 1:
+#         raise "Window size should be a positive number between 1 and size of the provided nums [1, len(nums)]"
+#     steps_num = len(nums) - k + 1
+#     if steps_num < 1:
+#         raise "Provided size of list should be equal or higher the size of window k"
+#
+#     result = []
+#     for i in range(steps_num):
+#         queue = deque(nums[i:k + i])
+#         result.append(max(queue))
+#
+#     return result
+
+def rebuild_deque(que, nums, idx, k):
+    while que:
+        smallest_kept_idx = que.pop()
+        if smallest_kept_idx < idx - k:
+            continue
+        if nums[smallest_kept_idx] > nums[idx]:
+            que.append(smallest_kept_idx)
+            que.append(idx)
+            break
+    if not que:
+        que.append(idx)
+
+def get_deque_max_idx(que):
+    max_idx = que.popleft()
+    que.appendleft(max_idx)
+    return max_idx
+
 def get_max_item_in_window(nums, k):
-    if k < 1:
-        raise "Window size should be a positive number between 1 and size of the provided nums [1, len(nums)]"
-    steps_num = len(nums) - k + 1
-    if steps_num < 1:
-        raise "Provided size of list should be equal or higher the size of window k"
-
+    if not 1 <= k <= len(nums):
+        raise ValueError("Window size k should be between 1 and len(nums) [1, len(nums)]")
     result = []
-    for i in range(steps_num):
-        queue = deque(nums[i:k + i])
-        result.append(max(queue))
-
+    que = deque()
+    for idx in range(len(nums)):
+        rebuild_deque(que, nums, idx, k)
+        if idx < k - 1:
+            continue
+        result.append(nums[get_deque_max_idx(que)])
     return result
 
-tests = [{
-    "payload": {
-        "nums": [1, 3, -1, -3, 5, 3, 6, 7],
-        "size": 3
-    },
-    "expected": [3, 3, 5, 5, 6, 7]
-}]
 
-for idx, t in enumerate(tests):
-    payload = t["payload"]
-    result = get_max_item_in_window(payload["nums"], payload["size"])
-    print(f"Test {idx + 1}")
-    if not result == t["expected"]:
-        print(f"Received {result} when {t["expected"]} was expected")
-    else:
-        print("OK")
-    print()
+
+def run_tests():
+    tests = [{
+        "payload": {
+            "nums": [1, 3, -1, -3, 5, 3, 6, 7],
+            "size": 3
+        },
+        "expected": [3, 3, 5, 5, 6, 7]
+    }]
+
+    for idx, t in enumerate(tests):
+        payload = t["payload"]
+        result = get_max_item_in_window(payload["nums"], payload["size"])
+        print(f"Test {idx + 1}")
+        if not result == t["expected"]:
+            print(f"Received {result} when {t["expected"]} was expected")
+        else:
+            print("OK")
+        print()
+
+run_tests()
